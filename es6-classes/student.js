6f604e95eb059f6d9590f57e46c9bee21d9ee327
Morris Wang <morris1214@gmail.com>
2022-12-09 04:05:39 +0000

    Amended es6-classes exercise

--- es6-classes/student.js
+++ es6-classes/student.js
@@ -6,15 +6,15 @@ class Student {
     this.lastName = lastName;
     this.subject = subject;
   }
+
+  getFullName() {
+    const { firstName, lastName } = this;
+    return `${firstName} ${lastName}`;
+  }
+
+  getIntroduction() {
+    const { subject } = this;
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+  }
 }
-
-Student.prototype.getFullName = function () {
-  const { firstName, lastName } = this;
-  return `${firstName} ${lastName}`;
-};
-
-Student.prototype.getIntroduction = function () {
-  const { subject } = this;
-  const fullName = this.getFullName();
-  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
-};
