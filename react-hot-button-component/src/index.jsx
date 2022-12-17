e5e728ed3376e78f832a2bf8ac8120aa159ac52a
Morris Wang <morris1214@gmail.com>
2022-12-17 23:12:31 +0000

    Completed react-hot-button component exercise

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+let clickCount = 0;
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+  }
+
+  handleClick(props) {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    clickCount++;
+    if (clickCount < 4) {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="cold">Hot Button</button>
+        </div>
+      );
+    } else if (clickCount < 7) {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="cool">Hot Button</button>
+        </div>
+      );
+    } else if (clickCount < 10) {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="luke-warm">Hot Button</button>
+        </div>
+      );
+    } else if (clickCount < 13) {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="warm">Hot Button</button>
+        </div>
+      );
+    } else if (clickCount < 16) {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="hot">Hot Button</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.handleClick.bind(this)} className="super-hot">Hot Button</button>
+        </div>
+      );
+    }
+  }
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<CustomButton />);
